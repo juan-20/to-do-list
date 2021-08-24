75ef5fdbd9023549ac00d18072f457d864bd58d4
Juan Andrade <49418565+juan-20@users.noreply.github.com>
2021-08-24 18:57:34 -0300

    Delete button

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import '../styles/tasklist.scss'
 
@@ -10,20 +10,43 @@ interface Task {
   isComplete: boolean;
 }
 
+let i: number = 0;
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle, setNewTaskTitle] = useState('');
-
+  let [newTaskTitle, setNewTaskTitle] = useState('');
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    console.log(newTaskTitle)
+    if (!newTaskTitle) {
+      console.log('empty')
+      return
+    }
+
+    const newTask = {
+      id: i++,
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    // o set demora pra renderizar então usa esse callback usando oldState. ele pega os valores atuais e colocam o newtask no fim igual um push
+    // se n adiciona e perde o ultimo
+    setTasks(oldState => [...oldState, newTask])
+    setNewTaskTitle('')
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    console.log(id);
+
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    console.log(id);
+    // essa const filtra dentro de todas as task  o id diferente do id
+    const deletedTasks = tasks.filter(tasks => tasks.id != id)
+    // quando vocÊ passa asism ele deleta
+    setTasks(deletedTasks)
   }
 
   return (
@@ -32,14 +55,14 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            type="text"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
           <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
-            <FiCheckSquare size={16} color="#fff"/>
+            <FiCheckSquare size={16} color="#fff" />
           </button>
         </div>
       </header>
@@ -50,7 +73,7 @@ export function TaskList() {
             <li key={task.id}>
               <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -62,11 +85,11 @@ export function TaskList() {
               </div>
 
               <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
-                <FiTrash size={16}/>
+                <FiTrash size={16} />
               </button>
             </li>
           ))}
-          
+
         </ul>
       </main>
     </section>
